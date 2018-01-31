4d981f96110311a994604d8d86b5a0e314457d10
ganesh <ganeshredcobra@gmail.com>
2018-01-31 14:37:48 +0530

    Return Json

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,39 @@
 from itertools import islice
 from flask import render_template
+from flask import jsonify
 from flask import request
 from app import app
 import requests
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Route',
+        'From': u'Trivandrum',
+		'Dest': u'Kovalam',
+		'Via': u'Statue',
+		'noStops':7,
+     	'done': u'False'
+    },
+    {
+        'id': 2,
+        'title': u'Ad',
+        'description': u'Smoking is Injurious', 
+        'done': u'False'
+    },
+	{
+		'id': 3,
+		'stops' : [
+		{u'No':0,u'Lat':10,u'Long':15,u'stop':u'tvm'},
+		{u'No':1,u'Lat':20,u'Long':25,u'stop':u'statue'},
+		{u'No':2,u'Lat':30,u'Long':35,u'stop':u'university'},
+		{u'No':3,u'Lat':40,u'Long':45,u'stop':u'pettah'},
+		{u'No':4,u'Lat':50,u'Long':55,u'stop':u'airport'},
+		{u'No':5,u'Lat':60,u'Long':65,u'stop':u'bypass'},
+		{u'No':6,u'Lat':60,u'Long':65,u'stop':u'kovalam'}		
+		]
+	}
+]
  
 values =[]
 title = "sample app"
@@ -22,7 +53,8 @@ def index():
 def push_data(): 
     print (request.json)
     values.append(request.json)
-    return "success.."
+    #return "success.."
+    return jsonify(tasks), 201
 
 @app.route('/data', methods=['GET'])
 def fetch_data(): 
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+export FLASK_DEBUG=1
+export FLASK_APP=json_handler.py
+flask run --host=0.0.0.0
